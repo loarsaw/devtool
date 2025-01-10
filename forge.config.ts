2bca5bcf1ca01911e2122febe6458f27e7ada870
loarsaw <amanahmed1385@gmail.com>
2025-01-10 13:04:02 +0530

    done

--- forge.config.ts
+++ forge.config.ts
@@ -24,7 +24,7 @@ const config: ForgeConfig = {
       config: {
         repository: {
           owner: 'loarsaw',
-          name: 'devtool'
+          name: 'hmi-ui-forge'
         },
         prerelease: true
       }
